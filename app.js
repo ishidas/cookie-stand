1ec3453e952d73fd91aa72ea699519257fdc37ec
Sawako Ishida <sawako.yoriko@gmail.com>
2015-11-03 17:26:52 -0800

    Got my list to be back on again..

--- app.js
+++ app.js
@@ -12,60 +12,63 @@ function CookieStand(storeLocation, minCustPerHr, maxCustPerHr, aveCookiesPerCus
 
   this.calculateRandomHrlyCust = function(){
     return Math.floor(Math.random()*(this.maxCustPerHr - this.minCustPerHr +1))+this.minCustPerHr;
+    console.log(this.calculateRandomHrlyCust());
   };
   this.calculateHourlyTtl = function(){
     return Math.ceil(this.calculateRandomHrlyCust() * this.aveCookiesPerCust);
+    console.log(this.calculateHourlyTtl());
   };
   this.calculateDailyTtl = function(){
     var pplist = document.getElementById('pikePlaceList');
     for (var i = 0; i < hoursOfOperation.length; i++) {
       this.hourlyCookies.push(this.calculateHourlyTtl());
       this.total += this.hourlyCookies[i];
+      console.log(this.total);
       var listItem1 = document.createElement("li");
       listItem1.innerHTML = hoursOfOperation[i]+ ": " + this.hourlyCookies[i]
       pplist.appendChild(listItem1)
     };
-      var listItem1 = document.createElement("li");
-      listItem1.innerHTML = "Total: " + this.hourlyCookies[i]
-      pplist.appendChild(this.total)
+      // var listItem1 = document.createElement("li");
+      // listItem1.innerHTML = "Total: " + this.hourlyCookies[i];
+      // pplist.appendChild(this.total);
   }
 };
 
 
-this.display = function() {
-  this.calculateHourlyTtl();
-  var table = document.createElement('table');
-  var row = document.createElement('tr');
-  for (var i = 0; i < hoursOfOperation.length; i++) {
-  var header = document.createElement('th');
-  var data = document.createElement('td');
-    data.textConent = hoursOfOperation[i];
-    header.appendChild(data);
-    row.appendChild(header);
-    console.log[i];
-  }
-  table.appendChild(row);
+// display = function() {
+//   this.calculateHourlyTtl();
+//   var table = document.createElement('table');
+//   var row = document.createElement('tr');
+//   for (var i = 0; i < hoursOfOperation.length; i++) {
+//   var header = document.createElement('th');
+//   var data = document.createElement('td');
+//     data.textConent = hoursOfOperation[i];
+//     header.appendChild(data);
+//     row.appendChild(header);
+//     console.log[i];
+//   }
+//   table.appendChild(row);
 
-  for (var i = o; i < this.hourlyCookies.length; i++) {
-    var row = document.createElement('tr');
-    var data = document.createElement('td');
-      data.textConent = this.hourlyCookies[i];
-      row.appendChild(data);
-      console.log[i];
-    }
-    table.appendChild(row);
+//   for (var i = o; i < this.hourlyCookies.length; i++) {
+//     var row = document.createElement('tr');
+//     var data = document.createElement('td');
+//       data.textConent = this.hourlyCookies[i];
+//       row.appendChild(data);
+//       console.log[i];
+//     }
+//     table.appendChild(row);
 
-  document.body.appendChild(row);
-
+//   document.body.appendChild(row);
+// }
 var pikePlace = new CookieStand('Pike Place Market', 17, 88, 5.2);
 var seaTacAirport = new CookieStand('SeaTac Airport', 6, 44, 1.2);
 var southCenterMall = new CookieStand('Southcenter Mall', 11, 38, 1.9);
 var bellevueSquare = new CookieStand('Bellevue Square Mall', 20, 48, 3.3);
 var alki = new CookieStand('Alki Beach', 3, 24, 2.6);
 
+// pikePlace.display();
 
-
-
+pikePlace.calculateDailyTtl();
 
 // //Pike Place store Hourly Customer, Hourly Cookies TTL, Daily TTL
 // var pikePlace = {
